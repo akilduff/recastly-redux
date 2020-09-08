5d085256853f1b7a151064190e892785c675638c
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-09-07 20:12:30 -0700

    Implement store file

--- src/store/store.js
+++ src/store/store.js
@@ -5,4 +5,6 @@ import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
 
-// const store = Redux.createStore(main);
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
